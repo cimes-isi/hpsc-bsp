a2d6215bbb961f0815b5717139a7751b3118928b
Connor Imes <cimes@isi.edu>
2020-02-27 09:26:30 -0500

    sdk/hpsc-eclipse: install GNU MCU plugin from archive

--- build-recipes/sdk/hpsc-eclipse.sh
+++ build-recipes/sdk/hpsc-eclipse.sh
@@ -7,6 +7,12 @@ export WGET_OUTPUT_MD5="6087e4def4382fd334de658f9bde190b"
 
 export DO_BUILD_OUT_OF_SOURCE=1
 
+# Required b/c there aren't any version-specific repositories available
+ECLIPSE_REPO_VER_GNU_MCU=4.5.1-201901011632
+ECLIPSE_REPO_ZIP_GNU_MCU=ilg.gnumcueclipse.repository-${ECLIPSE_REPO_VER_GNU_MCU}.zip
+ECLIPSE_REPO_SHA_GNU_MCU=bcac8d27b88989f021cd72ce900055e57cc395a3848baa805a27005d07060b14
+ECLIPSE_REPO_URL_GNU_MCU=https://github.com/gnu-mcu-eclipse/eclipse-plugins/releases/download/v${ECLIPSE_REPO_VER_GNU_MCU}/${ECLIPSE_REPO_ZIP_GNU_MCU}
+
 # Eclipse update sites
 # Use version-specific repos to prevent breakage when newer plugin versions are
 # released to the main update sites (the ones usually documented for users).
@@ -14,7 +20,10 @@ export DO_BUILD_OUT_OF_SOURCE=1
 # You may have to dig around a bit to find the correct URLs.
 ECLIPSE_REPOSITORIES=(
     "http://download.eclipse.org/tm/updates/4.5.0/repository/"
-    # "http://gnu-mcu-eclipse.netlify.com/v4-neon-updates/"
+    # TODO: This path is tied to the build system --> not portable!
+    #       Can't seem to get relative paths working
+    #       Should we deploy the archive instead and let users add local repo?
+    # "jar:file:${REC_SRC_DIR}/${ECLIPSE_REPO_ZIP_GNU_MCU}!/" # "http://gnu-mcu-eclipse.netlify.com/v4-neon-updates/"
     "http://downloads.yoctoproject.org/releases/eclipse-plugin/2.6.1/oxygen"
     "http://download.eclipse.org/linuxtools/update-7.1.0"
 )
@@ -43,6 +52,14 @@ ECLIPSE_HPSC=hpsc-${WGET_OUTPUT}
 DEPLOY_DIR=sdk
 DEPLOY_ARTIFACTS=("$ECLIPSE_HPSC")
 
+function eclipse_repos_fetch_local()
+{
+    env_maybe_wget "$ECLIPSE_REPO_URL_GNU_MCU" \
+                   "$ECLIPSE_REPO_ZIP_GNU_MCU" || return $?
+    env_check_sha256sum "$ECLIPSE_REPO_ZIP_GNU_MCU" \
+                        "$ECLIPSE_REPO_SHA_GNU_MCU"
+}
+
 function eclipse_p2_install_ius()
 {
     # Get repos and IUs as comma-delimited lists
@@ -60,6 +77,10 @@ function eclipse_p2_install_ius()
 
 function do_post_fetch()
 {
+    # Fetch versioned archives for repos that don't have versioned update sites
+    echo "hpsc-eclipse: fetching local plugin archives..."
+    eclipse_repos_fetch_local || return $?
+
     # Always re-create the eclipse directory and re-fetch plugins.
     # This avoids problems when plugins are incrementally upgraded or removed.
     # We could handle the upgrade case, but there's a non-trivial likelihood of
